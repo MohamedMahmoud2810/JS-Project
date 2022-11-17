0ff620fd808a2e057601af26c08fa784a6854b62
Mohamed Mahmoud <mohammedmahmmoud05@gmail.com>
2022-11-17 16:25:35 +0200

    uploaded

---
+++ index.js
@@ -0,0 +1,15 @@
+var fname = window.prompt("Enter Your First Name ? ");
+var lname = window.prompt("Enter Your Last Name ? ");
+var FullName = fname + " " + lname;
+
+var age = window.prompt("Enter Your Age ?");
+
+alert("Welcome " + FullName + " " + "you are " + age + " " + "Years old");
+
+
+alert("this is the first release of a calculator that only has a summation feature . ");
+
+var FirstNumber =Number(window.prompt("Enter The First Number : ")) ;
+var LastNumber = Number(window.prompt("Enter The Last Number : "));
+
+alert("the result is : "+ (FirstNumber  + LastNumber));
